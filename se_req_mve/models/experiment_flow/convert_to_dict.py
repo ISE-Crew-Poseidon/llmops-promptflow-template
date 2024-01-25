17ffef304a856eab1a764bf11a1384b1670b06ad
Marcus Clendenin <mclendenin@microsoft.com>
2024-01-25 15:14:34 -0500

    Fix output path for mve model

--- se_req_mve/models/experiment_flow/convert_to_dict.py
+++ se_req_mve/models/experiment_flow/convert_to_dict.py
@@ -11,8 +11,15 @@ logger = llmops_logger()
 def convert_to_dict(input_str: str) -> dict:
     """Converts the raw text output from the LLM into a dictionary."""
     try:
-        return json.loads(input_str)
+        response = json.loads(input_str)
+        assert "violation" in response
+        response["violation"] = response["violation"].lower() in [
+            "yes",
+            "correct",
+            "true",
+        ]
+        return response
 
     except Exception as e:
-        logger.error("The input is not valid, error: {}".format(e))
+        print("The input is not valid, error: {}".format(e))
         return {"violation": None, "reason": "Failed to parse LLM output."}
