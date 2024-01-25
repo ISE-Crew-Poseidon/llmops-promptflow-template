e12e457545f51ca77799fe40b763279625e49636
Marcus Clendenin <mclendenin@microsoft.com>
2024-01-25 14:50:06 -0500

    Update prompt_eval to use runtime

--- llmops/common/prompt_eval.py
+++ llmops/common/prompt_eval.py
@@ -137,12 +137,12 @@ def prepare_and_execute(
                 data=data_id,
                 run=my_run,
                 column_mapping=mapping_node,
-                # runtime=runtime,
+                runtime=runtime,
                 # un-comment the resources line and
                 # comment the argument runtime to
                 # enable automatic runtime.
                 # Reference: COMPUTE_RUNTIME
-                resources={"instance_type": "Standard_E4ds_v4"},
+                # resources={"instance_type": "Standard_E4ds_v4"},
                 name=f"{experiment_name}_eval_{timestamp}",
                 display_name=f"{experiment_name}_eval_{timestamp}",
                 tags={"build_id": build_id},
@@ -152,9 +152,6 @@ def prepare_and_execute(
             df_result = None
 
             time.sleep(15)
-            if eval_job.status == "NotStarted":
-                logger.info("Job didn't start in time, retry once")
-                eval_job = pf.runs.create_or_update(eval_run, stream=True)
             if eval_job.status == "Completed" or eval_job.status == "Finished":
                 logger.info(eval_job.status)
                 df_result = pf.get_details(eval_job)
